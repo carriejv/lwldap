c0fc865a53f6ded1f29aa869e80f5da1b5f23644
Carrie J V <c@cjv.sh>
2026-01-05 10:00:39 +0100

    startup updates

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,18 +1,40 @@
 #!/bin/bash
 
-set -euo pipefail
+set -eo pipefail
+
+LDIF_DIR=/ldif
+SLAPD_CFG_DIR=/etc/openldap/slapd.d
 
 function log() {
-    echo "[lwldap][$(date)] - $1"
+    echo "[lwldap][$(date)] $1"
 }
 
-if [ -z "${LWLDAP_DEBUG_STARTUP+1}" ]; then
+function should_config() {
+    if [ ! -z "${LWLDAP_SKIP_CONFIG}" ]; then
+        echo "Skipped config setup due to flag."
+        return
+    fi
+    if [ ! -f "$LDIF_DIR/config.ldif" ]; then
+        echo "Skipped config setup because $LDIF_DIR/config.ldif does not exist."
+        return
+    fi
+    if [ -n "$(ls -A $SLAPD_CFG_DIR 2>/dev/null)" ]; then
+        echo "Skipped config setup because config already exists."
+        return
+    fi
+}
+
+# debug flag
+if [ ! -z "${LWLDAP_DEBUG_STARTUP}" ]; then
     set -x
 fi
 
 # config.ldif init
-if [ -z "${LWLDAP_DEBUG_STARTUP}" ]; then
+should_config_err="$(should_config)"
+if [ -z "$should_config_err" ]; then
+    echo "foo"
+    exit 99
     slapadd -n 0 -F /etc/openldap/slapd.d/ -l /ldif/config.ldif
 else
-    log "Skipped config setup due to flag."
+    log "$should_config_err"
 fi
