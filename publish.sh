5ea18064b16d233ba631468b51169c8da20fc9b8
Carrie J V <c@cjv.sh>
2026-04-06 01:56:12 +0200

    0.3.0: add arm64 build

--- publish.sh
+++ publish.sh
@@ -8,10 +8,10 @@ set -euo pipefail
 TARGETS=("latest" "$1")
 for tgt in "${TARGETS[@]}"; do
     echo "Building "$tgt"..."
-    docker build . -t "carriejv/lwldap:$tgt"
+    docker build . --platform linux/amd64,linux/arm64 -t "carriejv/lwldap:$tgt"
     docker push "carriejv/lwldap:$tgt"
     tgt55="$tgt-55"
     echo "Building "$tgt55"..."
-    docker build . --build-arg LWLDAP_UID=55 --build-arg LWLDAP_GID=55 -t "carriejv/lwldap:$tgt55"
+    docker build . --build-arg LWLDAP_UID=55 --build-arg LWLDAP_GID=55 --platform linux/amd64,linux/arm64 -t "carriejv/lwldap:$tgt55"
     docker push "carriejv/lwldap:$tgt55"
 done
